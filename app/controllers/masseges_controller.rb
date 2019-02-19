7a26c8a72b9be5986aa6690eafaf4374b7917779
paradox <paradox2028@gmail.com>
2019-02-20 02:05:14 +0900

    g model&controller

---
+++ app/controllers/masseges_controller.rb
@@ -0,0 +1,2 @@
+class MassegesController < ApplicationController
+end
---
+++ test/controllers/masseges_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MassegesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
