1bca0964a45cbced258842a11b03a9dcbf4f8028
paradox2028 <paradox2028@gmail.com>
2019-02-26 23:45:26 +0900

    edit app/model/message.rb add validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }
+  validates :username, presence: true, length: { minimum: 1 }
+  validates :content, presence: true, length: { minimum: 1 }
 end
